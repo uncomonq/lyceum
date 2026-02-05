74772882a1924344de7945f6d9a4c99c5206befb
uncomonq <uncomonq@mail.ru>
2026-02-05 11:57:06 +0300

    fix quotes

--- lyceum/about/views.py
+++ lyceum/about/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def about_view(request):
-    return render(request, 'about/about.html')
+    return render(request, "about/about.html")
--- lyceum/homepage/views.py
+++ lyceum/homepage/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def homepage_view(request):
-    return render(request, 'homepage/homepage.html')
+    return render(request, "homepage/homepage.html")
