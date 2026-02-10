c0206bd76b5c3d1542ed9e1c7ab5812215bd3a93
uncomonq <uncomonq@mail.ru>
2026-02-10 23:41:23 +0300

    reformate amd flake8

--- lyceum/catalog/urls.py
+++ lyceum/catalog/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path("<int:item_id>/", catalog.views.item_detail),
     re_path(r"^re/(?P<number>[1-9][0-9]*)/$", catalog.views.return_value_view),
     re_path(
-        r"^converter/(?P<number>[1-9][0-9]*)/$", catalog.views.return_value_view
+        r"^converter/(?P<number>[1-9][0-9]*)/$",
+        catalog.views.return_value_view,
     ),
 ]
