002b32c0d075727c9efd1aaf9494322f8670875b
uncomonq <uncomonq@mail.ru>
2026-02-11 00:11:14 +0300

    black reformate

--- lyceum/catalog/urls.py
+++ lyceum/catalog/urls.py
@@ -9,6 +9,8 @@ register_converter(PositiveIntWithLeadingZerosConverter, "posint")
 urlpatterns = [
     path("", catalog.views.item_list),
     path("<int:item_id>/", catalog.views.item_detail),
-    re_path(r"^re/(?P<number>0*[1-9][0-9]*)/$", catalog.views.return_value_view),
+    re_path(
+        r"^re/(?P<number>0*[1-9][0-9]*)/$", catalog.views.return_value_view
+    ),
     path("converter/<posint:number>/", catalog.views.return_value_view),
 ]
--- lyceum/catalog/views.py
+++ lyceum/catalog/views.py
@@ -11,4 +11,6 @@ def item_detail(request, item_id):
 
 def return_value_view(request, number):
     number_int = int(number)
-    return HttpResponse(str(number_int), content_type="text/plain; charset=utf-8")
+    return HttpResponse(
+        str(number_int), content_type="text/plain; charset=utf-8"
+    )
