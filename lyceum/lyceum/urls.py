923c76ace544248703f520f9332236f63b16c272
uncomonq <uncomonq@mail.ru>
2026-02-15 16:00:54 +0300

    fix imports

--- lyceum/lyceum/urls.py
+++ lyceum/lyceum/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
-from django.contrib import admin
 from debug_toolbar.toolbar import debug_toolbar_urls
+from django.contrib import admin
 from django.urls import include, path
 
 
