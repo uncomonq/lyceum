4c12a03d6e2f90cbc839553ea184421a7e6cd104
uncomonq <uncomonq@mail.ru>
2026-02-17 05:07:49 +0300

    fix admin

--- lyceum/catalog/admin.py
+++ lyceum/catalog/admin.py
@@ -2,24 +2,19 @@ from django.contrib import admin
 
 import catalog.models
 
-COMMON_DISPLAY = ("name", "is_published")
-COMMON_EDITABLE = ("is_published",)
-CATEGORY_DISPLAY = ("name", "weight", "is_published")
-CATEGORY_EDITABLE = ("weight", "is_published")
-
 
 @admin.register(catalog.models.CatalogItem)
 class ItemAdmin(admin.ModelAdmin):
-    list_display = COMMON_DISPLAY
-    list_editable = COMMON_EDITABLE
+    list_display = ("name", "is_published")
+    list_editable = ("is_published",)
     list_display_links = ("name",)
     filter_horizontal = ("tags",)
 
 
 @admin.register(catalog.models.CatalogCategory)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = CATEGORY_DISPLAY
-    list_editable = CATEGORY_EDITABLE
+    list_display = ("name", "weight", "is_published")
+    list_editable = ("weight", "is_published")
     search_fields = ("name",)
 
 
@@ -30,5 +25,5 @@ class TagAdmin(admin.ModelAdmin):
         return obj.name
 
     list_display = ("get_name", "is_published")
-    list_editable = COMMON_EDITABLE
+    list_editable = ("is_published",)
     search_fields = ("name",)
