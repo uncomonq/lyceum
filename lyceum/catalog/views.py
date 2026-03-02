f2ce02120615347a8869815062627a70bd00420c
uncomonq <uncomonq@mail.ru>
2026-03-02 22:12:11 +0300

    use prefetch

--- lyceum/catalog/views.py
+++ lyceum/catalog/views.py
@@ -1,4 +1,5 @@
 __all__ = ("item_list", "item_detail", "return_value_view")
+import django.db.models
 import django.http
 import django.shortcuts
 
@@ -8,9 +9,19 @@ import catalog.models
 def item_list(request):
     templates = "catalog/item_list.html"
     items = (
-        catalog.models.Item.objects.filter(is_published=True)
+        catalog.models.Item.objects.filter(
+            is_published=True,
+            category__is_published=True,
+        )
         .select_related("category")
-        .prefetch_related("tags")
+        .prefetch_related(
+            django.db.models.Prefetch(
+                "tags",
+                queryset=catalog.models.Tag.objects.filter(
+                    is_published=True,
+                ).only("name"),
+            ),
+        )
         .only("name", "text", "category__name")
         .order_by("category__name")
     )
@@ -24,8 +35,28 @@ def item_list(request):
 def item_detail(request, pk):
     templates = "catalog/item.html"
     item = django.shortcuts.get_object_or_404(
-        catalog.models.Item.objects.select_related("category", "main_image")
-        .prefetch_related("tags", "images")
+        catalog.models.Item.objects.filter(
+            is_published=True,
+            category__is_published=True,
+        )
+        .select_related("category", "main_image")
+        .prefetch_related(
+            django.db.models.Prefetch(
+                "tags",
+                queryset=catalog.models.Tag.objects.filter(
+                    is_published=True,
+                ).only("name"),
+            ),
+            django.db.models.Prefetch(
+                "images",
+                queryset=catalog.models.ItemImage.objects.only(
+                    "image",
+                    "alt",
+                    "ordering",
+                    "item_id",
+                ),
+            ),
+        )
         .only(
             "name",
             "text",
--- lyceum/homepage/views.py
+++ lyceum/homepage/views.py
@@ -1,6 +1,7 @@
 __all__ = ("home", "coffee")
 import http
 
+import django.db.models
 from django.http import HttpResponse
 import django.shortcuts
 
@@ -10,9 +11,20 @@ import catalog.models
 def home(request):
     templates = "homepage/main.html"
     items = (
-        catalog.models.Item.objects.filter(is_published=True, is_on_main=True)
+        catalog.models.Item.objects.filter(
+            is_published=True,
+            is_on_main=True,
+            category__is_published=True,
+        )
         .select_related("category")
-        .prefetch_related("tags")
+        .prefetch_related(
+            django.db.models.Prefetch(
+                "tags",
+                queryset=catalog.models.Tag.objects.filter(
+                    is_published=True,
+                ).only("name"),
+            ),
+        )
         .order_by("name")
     )
     return django.shortcuts.render(
